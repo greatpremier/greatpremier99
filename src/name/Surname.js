f955b00eb08918bdde2801a3f6bfdc19baf5e81b
Tanaka Kakono <pittkayz@gmail.com>
2024-07-19 23:28:14 +0200

    Second commit

---
+++ src/name/Surname.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Surname extends React.Component {
+    render(){
+        return (
+            <h1>The Surname is kakono</h1>
+        )
+    }
+}
+
+export default Surname;
